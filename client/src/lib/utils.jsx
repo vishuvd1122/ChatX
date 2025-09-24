824e15c2bb88df29f1a1530039079c006994a5d0
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-09-24 14:40:46 +0530

    Update Chat Container
    Updated the container from scratch using the hardcoded data inside assets folder

---
+++ client/src/lib/utils.jsx
@@ -0,0 +1,6 @@
+export function formatMessageTime(date){
+    const d = new Date(date);
+    const hours = d.getHours().toString().padStart(2, '0');
+    const minutes = d.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
