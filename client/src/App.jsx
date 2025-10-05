613bf40c8edf043826756f7aa94cd5586e99312d
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-10-05 14:44:28 +0530

    Minor UI changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,24 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 
-
-
 const App = () => {
   return (
-    <div className='bg-[url("./src/assets/bgImage.svg")] bg-contain'>
-      <Routes>
-        <Route path='/' element = {<HomePage/>}/>
-        <Route path='/login' element = {<LoginPage/>}/>
-        <Route path='/profile' element = {<ProfilePage/>}/>
-      </Routes>
+    <div className="app-root min-h-screen relative text-white overflow-hidden">
+      {/* Animated background layers */}
+      <div className="animated-bg absolute inset-0 -z-10"></div>
+      <div className="floating-orbs absolute inset-0 -z-5"></div>
+      
+      {/* Content overlay */}
+      <div className="bg-overlay absolute inset-0 -z-1 pointer-events-none"></div>
+
+      {/* App content */}
+      <div className="relative z-10">
+        <Routes>
+          <Route path='/' element = {<HomePage/>}/>
+          <Route path='/login' element = {<LoginPage/>}/>
+          <Route path='/profile' element = {<ProfilePage/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
